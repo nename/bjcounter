d18db99570fd564489d68b509ef7e09e58863109
Tomas <xhampl10@vutbr.cz>
2021-01-12 15:53:06 +0100

    added key controls, number of decks set
    arrow keys now work as controls and you can set number of decks the game is played with

--- counter.py
+++ counter.py
@@ -16,15 +16,21 @@ from tkinter import *
 # mine libraries
 import cheatsheet as chs
 
-# global counters
+# globals
 runningCount = cardCount = deckCount = trueCount = 0
-
-# global cards
 firstCard = secCard = dealerCard = 0
+numOfDecs = 8
 
 # functions
+# calculate true count
+def calculateTrueCount(deckCount, runningCount):
+    global cardCount, trueCount
+
+    cardCount = cardCount + 1
+    trueCount = round(runningCount / (float(numOfDecs) - (deckCount + cardCount / 52)), 2)
+
 # add 1 to deck count
-def deckAddCount():
+def deckAddCount(*args):
     global cardCount, deckCount
 
     cardCount = 0
@@ -34,11 +40,12 @@ def deckAddCount():
     deckCountLabel.configure(text=f'{deckCount}')
 
 # add 1 to running count and to card count
-def clickAdd():
+def clickAdd(*args):
     global runningCount, cardCount, deckCount, trueCount
-    cardCount = cardCount + 1
+
     runningCount = runningCount + 1
-    trueCount = runningCount / (8 - deckCount)
+
+    calculateTrueCount(deckCount, runningCount)
 
     if cardCount >= 52:
         deckAddCount()
@@ -48,11 +55,12 @@ def clickAdd():
     trueCountLabel.configure(text=f'{trueCount}')
 
 # add -1 to running count and 1 to card count
-def clickMinus():
+def clickMinus(*args):
     global runningCount, cardCount, deckCount, trueCount
-    cardCount = cardCount + 1
+
     runningCount = runningCount - 1
-    trueCount = runningCount / (8 - deckCount)
+
+    calculateTrueCount(deckCount, runningCount)
 
     if cardCount >= 52:
         deckAddCount()
@@ -62,10 +70,10 @@ def clickMinus():
     trueCountLabel.configure(text=f'{trueCount}')
 
 # add 1 to card count
-def clickNeutral():
+def clickNeutral(*args):
     global cardCount, deckCount, trueCount
-    cardCount = cardCount + 1
-    trueCount = runningCount / (8 - deckCount)
+
+    calculateTrueCount(deckCount, runningCount)
 
     if cardCount >= 52:
         deckAddCount()
@@ -74,7 +82,7 @@ def clickNeutral():
     trueCountLabel.configure(text=f'{trueCount}')
 
 # reset counters
-def reset():
+def reset(*args):
     global runningCount, cardCount, deckCount, trueCount
     runningCount = cardCount = deckCount = trueCount = 0
 
@@ -92,16 +100,12 @@ def setCardValues(*args):
     dealerCard = clicked3.get()
 
 # display advice
-def giveAdvice():
+def giveAdvice(*args):
     global firstCard, secCard, dealerCard
 
-    print(firstCard, secCard, dealerCard)
-
     if not firstCard or not secCard or not dealerCard or firstCard == 0 or secCard == 0 or dealerCard == 0:
-        adviceResLabel.configure(text="0")
-        return
+        return adviceResLabel.configure(text=" ")
 
-    advice = ""
     indexDealer = chs.dropDownOptions.index(dealerCard)
 
     # split pair
@@ -130,88 +134,130 @@ def giveAdvice():
 
     adviceResLabel.configure(text=f'{advice}')
 
+# set number of decs
+def setNumOfDecs(*args):
+    global numOfDecs, cardCount, deckCount
+
+    if cardCount > 0 or deckCount > 0:
+        return entryBox.config({"background": "Red"})
+
+    numOfDecs = entryBox.get()
+    entryBox.config({"background": "White"})
+    if int(numOfDecs) <= 0:
+        entryBox.config({"background": "Red"})
+        numOfDecs = 8
+
 # init
 windows = tk.Tk()
 windows.title("BJ counter")
-windows.geometry("340x310")
+windows.geometry("360x350")
 
 # labels for counts
 runningLabel = tk.Label(windows, text="running count:")
-runningLabel.grid(column=0, row=1)
+runningLabel.grid(column=0, row=2)
 
 runningCountLabel = tk.Label(windows, text="0")
-runningCountLabel.grid(column=1, row=1)
+runningCountLabel.grid(column=1, row=2)
 
 cardLabel = tk.Label(windows, text="card count:")
-cardLabel.grid(column=0, row=2)
+cardLabel.grid(column=0, row=3)
 
 cardCountLabel = tk.Label(windows, text="0")
-cardCountLabel.grid(column=1, row=2)
+cardCountLabel.grid(column=1, row=3)
 
 deckLabel = tk.Label(windows, text="deck count:")
-deckLabel.grid(column=0, row=3)
+deckLabel.grid(column=0, row=4)
 
 deckCountLabel = tk.Label(windows, text="0")
-deckCountLabel.grid(column=1, row=3)
+deckCountLabel.grid(column=1, row=4)
 
 trueLabel = tk.Label(windows, text="true count:")
-trueLabel.grid(column=0, row=4)
+trueLabel.grid(column=0, row=5)
 
 trueCountLabel = tk.Label(windows, text="0")
-trueCountLabel.grid(column=1, row=4)
+trueCountLabel.grid(column=1, row=5)
 
 # labels for cards
 firstCardLabel = tk.Label(windows, text="first card")
-firstCardLabel.grid(column=0, row=6)
+firstCardLabel.grid(column=0, row=7)
 
 secCardLabel = tk.Label(windows, text="second card")
-secCardLabel.grid(column=1, row=6)
+secCardLabel.grid(column=1, row=7)
 
 dealerCardLabel = tk.Label(windows, text="dealers card")
-dealerCardLabel.grid(column=2, row=6)
+dealerCardLabel.grid(column=2, row=7)
 
 # labels for advice
-adviceLabel = tk.Label(windows, text="next move")
-adviceLabel.grid(column=0, row=9)
+adviceLabel = tk.Label(windows, text="next move:")
+adviceLabel.grid(column=0, row=10)
 
 adviceResLabel = tk.Label(windows)
-adviceResLabel.grid(column=1, row=9)
+adviceResLabel.grid(column=1, row=10)
 
+# labels for hint
 hintLabel = tk.Label(windows, text="H - hit\nS - stand\nD - double\\hit\nDs - double\\stand\nN - do not split\n"
                                    "Y - split")
-hintLabel.grid(column=1, row=10)
+hintLabel.grid(column=0, row=11, columnspan=2)
+
+# labels for controls
+controlLabel = tk.Label(windows, text="controls:\nup - high num\ndown - low num\nright - neut num\nleft - neut num\n"
+                        "r - reset")
+controlLabel.grid(column=2, row=11)
+
+# labels for entry
+entryLabel = tk.Label(windows, text="Number of decks\nDefault 8")
+entryLabel.grid(column=0,row=0)
 
 # buttons +, 0, -, reset
 lowNumButton = ttk.Button(windows, text="2-6", command=clickAdd)
-lowNumButton.grid(column=0, row=0)
+lowNumButton.grid(column=0, row=1)
 
 neutralNumButton = ttk.Button(windows, text="7-9", command=clickNeutral)
-neutralNumButton.grid(column=1, row=0)
+neutralNumButton.grid(column=1, row=1)
 
 highNumButton = ttk.Button(windows, text="10-A", command=clickMinus)
-highNumButton.grid(column=2, row=0)
+highNumButton.grid(column=2, row=1, padx=10)
 
 resetCountsButton = ttk.Button(windows, text="reset", command=reset)
-resetCountsButton.grid(column=3, row=0)
+resetCountsButton.grid(column=3, row=1)
 
 # buttons for advice
 adviceButton = ttk.Button(windows, text="advice", command=giveAdvice)
-adviceButton.grid(column=1, row=8)
+adviceButton.grid(column=1, row=9)
+
+# buttons for entry box
+entryButton = ttk.Button(windows, text="set", command=setNumOfDecs)
+entryButton.grid(column=2, row=0)
 
 # dropdown for cards
-clicked = StringVar()
+clicked1 = StringVar()
+clicked2 = StringVar()
+clicked3 = StringVar()
 
-dropDown1 = OptionMenu(windows, clicked, *chs.dropDownOptions)
-dropDown1.grid(column=0, row=7)
+dropDown1 = OptionMenu(windows, clicked1, *chs.dropDownOptions)
+dropDown1.grid(column=0, row=8)
 
-dropDown2 = OptionMenu(windows, clicked, *chs.dropDownOptions)
-dropDown2.grid(column=1, row=7)
+dropDown2 = OptionMenu(windows, clicked2, *chs.dropDownOptions)
+dropDown2.grid(column=1, row=8)
 
-dropDown3 = OptionMenu(windows, clicked, *chs.dropDownOptions)
-dropDown3.grid(column=2, row=7)
+dropDown3 = OptionMenu(windows, clicked3, *chs.dropDownOptions)
+dropDown3.grid(column=2, row=8)
 
-clicked.trace('w', setCardValues)
+clicked1.trace('w', setCardValues)
+clicked2.trace('w', setCardValues)
+clicked3.trace('w', setCardValues)
+
+# binding key presses to buttons
+windows.bind('<Up>', clickMinus)
+windows.bind('<Down>', clickAdd)
+windows.bind('<Left>', clickNeutral)
+windows.bind('<Right>', clickNeutral)
+windows.bind('<r>', reset)
+
+# entry box for deck count
+entryBox = tk.Entry(windows, width=10, justify='center')
+entryBox.grid(column=1, row=0)
+entryBox.insert(0, "8")
 
 # main loop
-
 windows.mainloop()
